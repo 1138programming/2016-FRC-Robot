7b241f69c3c5b2cebefd0099a22189bc3e4f3d42
Pauline <eeuser@HS-EE-14.chaminet.net>
2017-02-19 11:20:19 -0800

    Still trying to commit the esophagus

--- src/Commands/EsophagusToBallCollection.cpp
+++ src/Commands/EsophagusToBallCollection.cpp
@@ -26,7 +26,7 @@ void EsophagusToBallCollection::Execute()
 // Make this return true when this Command no longer needs to run execute()
 bool EsophagusToBallCollection::IsFinished()
 {
-	return false;
+	return true;
 }
 
 // Called once after isFinished returns true
