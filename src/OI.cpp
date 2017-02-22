7a14a94c4ea671f8a8bcd43f89183ef58f5c28a3
eeuser <eeuser@HS-EE-12.chaminet.net>
2017-02-21 20:52:00 -0800

    Jahred and Christian 2/21/2017
    Finished OI

--- src/OI.cpp
+++ src/OI.cpp
@@ -1,7 +1,64 @@
 #include "OI.h"
+#include "Commands/ShiftBase.h"
+#include "Commands/ShiftEsophagus.h"
+#include "Commands/VisionTrackingCommand.h"
+#include "Commands/ShiftFlywheels.h"
+#include "Commands/FlywheelsIncreaseSpeed.h"
+#include "Commands/FlywheelsDecreaseSpeed.h"
 
-#include <WPILib.h>
 
-OI::OI() {
-	// Process operator interface input here.
+OI::OI()
+{
+	//Logitech and xbox controllers
+	leftController = new Joystick(0) ;
+	rightController = new Joystick(1) ;
+	xBoxController = new Joystick(2) ;
+
+	//Logitech Buttons
+	shiftButton = new JoystickButton(rightController, 1) ; //Shifts the Base from Low Gear to High Gear and vice versa
+	liftButton = new JoystickButton(leftController, 0) ;   //Shifts the Base to Lift the robot and vice versa
+
+	//XBox Definitions
+	buttonA	= new JoystickButton(xBoxController, 1) ;	//Toggle Shooter
+	buttonB = new JoystickButton(xBoxController, 2) ;	//Toggle Vision
+	buttonX = new JoystickButton(xBoxController, 3) ;	//Toggle Esophagus
+	buttonLB = new JoystickButton(xBoxController, 5) ;	//Decrease Flywheel Speed
+	buttonRB = new JoystickButton(xBoxController, 6) ;	//Increase Flywheel Speed
+
+	shiftButton->WhenPressed(new ShiftBase());
+	liftButton->WhenPressed(new ToggleLift());
+	buttonX->WhenPressed(new ShiftEsophagus());
+	buttonB->WhenPressed(new VisionTrackingCommand());
+	buttonA->WhenPressed(new ShiftFlywheels());
+	buttonLB->WhenPressed(new FlywheelsDecreaseSpeed());
+	buttonRB->WhenPressed(new FlywheelsIncreaseSpeed());
+}
+
+float OI::getRightController(){			//Right controller is right side drive
+	return rightController->GetY();
+}
+
+float OI::getLeftController(){			//Left controller is left side drive
+	return leftController->GetY();
+}
+
+bool OI::getLeftTrigger(){				//left controller's trigger is the shifter
+	return liftButton->Get();
+}
+
+bool OI::getRightTrigger(){			//right controller's trigger reverses the base direction
+	return shiftButton->Get();
+}
+
+float OI::getLeftXBoxAxis(){			//left xbox axis controls the shooter cage pivot
+	return -xBoxController->GetRawAxis(1);
+}
+
+//controls the collector arms, signal from joystick is reversed
+float OI::getRightXBoxAxis(){
+	return -xBoxController->GetRawAxis(5);
+}
+
+int OI::getXBoxPOV(){					//POV controls the collector arms
+	return xBoxController->GetPOV();
 }
--- src/OI.h
+++ src/OI.h
@@ -1,9 +1,41 @@
 #ifndef OI_H
 #define OI_H
 
-class OI {
-public:
-	OI();
-};
+#include "WPILib.h"
 
-#endif  // OI_H
+
+class OI
+{
+
+private:
+		//Three joysticks -- 2 logitech; 1 xbox
+		Joystick* leftController ;	//Left Drive Base
+		Joystick* rightController ;	//Right Drive Base
+		Joystick* xBoxController ;	//Runs Collector
+
+public:
+		//Logitech Button Definitions
+		JoystickButton* shiftButton ; //Right Trigger
+		JoystickButton* liftButton ; //Left Trigger
+
+		//XBox Button Definitions
+		JoystickButton* buttonA ;		//Toggle Shooter System
+		JoystickButton* buttonB ;		//Toggle Vision Tracking
+		JoystickButton* buttonX ;		//Toggle Esophagus
+		JoystickButton* buttonLB ;		//Decrease Flywheel Speed
+		JoystickButton* buttonRB ;		//Increase Flywheel Speed
+		//JoystickButton* buttonSelect ;
+		//JoystickButton* buttonStart ;
+
+	OI();
+	float getRightController() ;		//Right Controller is the Right Side Drive
+	float getLeftController() ;			//Left Controller is the Left Side Drive
+	bool getLeftTrigger() ;
+	bool getRightTrigger() ;
+	float getLeftXBoxAxis() ;			//Controls collector roller - up = intake; down = outtake
+	float getRightXBoxAxis() ;			//No function
+	int getXBoxPOV() ;					//No function
+
+} ;
+
+#endif
