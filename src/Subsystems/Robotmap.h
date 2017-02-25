7f97fcc893265d7607cafb91cad268b7d394995a
eeuser <eeuser@HS-EE-08.chaminet.net>
2017-02-25 13:51:28 -0800

    This is the Robot map for the assignments of talons and sensors.

---
+++ src/Subsystems/Robotmap.h
@@ -0,0 +1,99 @@
+#ifndef SRC_SUBSYSTEMS_DRIVEBASE_H_
+#define SRC_SUBSYSTEMS_DRIVEBASE_H_
+
+#include "Commands/Subsystem.h"
+#include "WPILib.h"
+#include "CANTalon.h"
+#include "c:\Users\eeuser\wpilib\cpp\current\include\Ultrasonic.h"
+#include "C:\Users\eeuser\wpilib\cpp\current\include\SmartDashboard\SmartDashboard.h"
+
+class DriveBase: public Subsystem
+{
+private:
+
+	//Subsystem Talons and Solenoids
+		//Collector
+		CANTalon* collectorCIM;
+
+		//Drive Base Talons and Solenoids
+		CANTalon* LeftRearBaseMotor; //Talon 1 in CAN Bus
+		CANTalon* LeftFrontBaseMotor; //Talon 2 in CAN Bus
+		CANTalon* RightRearBaseMotor; //Talon 3 in CAN Bus
+		CANTalon* RightFrontBaseMotor; //Talon 4 in CAN Bus
+
+		//Flywheel Talons
+		CANTalon* flywheelRight;
+		CANTalon* flywheelLeft;
+		CANTalon* filterFront;
+		CANTalon* filterRear;
+
+		//Solenoids
+		//Drive Base Solenoids
+		DoubleSolenoid* shiftSolenoid;
+		DoubleSolenoid* liftSolenoid;
+
+		//Esophagus Solenoid
+		DoubleSolenoid* esophagusSolenoid;
+
+		//Joysticks
+		Joystick* l_stick; //Logitech Joystick
+		Joystick* r_stick; //Logitech Joystick
+		Joystick* x_stick; //X-Box Joystick
+
+		//Sensors
+		//NavX MXP
+		AHRS* ahrs;
+
+			//Ultrasonic
+		Ultrasonic* BaseUltrasonic;
+
+		PowerDistributionPanel* myPDP;
+		//Live Window
+		//LiveWindow* lw = LiveWindow::GetInstance();
+
+		//USB Camera
+		static CameraServer* GetInstance();
+
+		//update every 0.005 seconds/5 milliseconds.
+		double kUpdatePeriod = 0.005;
+
+		const float KRamp = 0.6;
+
+public:
+
+	//Talons
+	const int RightFrontBaseTalon = 1;
+	const int RightRearBaseTalon = 3;
+	const int LeftFrontBaseTalon = 2;
+	const int LeftRearBaseTalon = 4;
+	const int flywheelLeftTalon = 8;
+	const int flywheelRightTalon = 7;
+	const int filterFrontTalon = 5;
+	const int filterRearTalon = 6;
+	const int CollectorTalon = 9;
+
+	//Solenoids
+	const int shiftSolenoidForward = 0;
+	const int shiftSolenoidReverse = 1;
+	const int liftSolenoidForward = 2;
+	const int liftSolenoidReverse = 3;
+	const int esophagusSolenoidForward = 4;
+	const int esophagusSolenoidReverse = 5;
+
+	//Joysticks
+	const int JoystickPort1 = 1;
+	const int JoystickPort0 = 0;
+	const int JoystickPort2 = 2;
+
+	//Ultrasonic
+	const int BaseUltrasonicTransmitter = 1;
+	const int BaseUltrasonicReciever = 1;
+
+	//Gyro
+	const float ahrsPort = SPI::Port::kMXP;
+
+	//PDP
+	const int PDPPort = 0;
+};
+
+#endif /* SRC_SUBSYSTEMS_BASE_H_ */
