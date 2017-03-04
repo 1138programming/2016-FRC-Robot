8b0d68bbd2254f195a8aa4c3ad27dfe5cfd892b7
eeuser <eeuser@HS-EE-12.chaminet.net>
2017-03-04 15:18:34 -0800

    Both Autonomous Completed
    Both of the autonomi are now completed, but still not implemented in
    CommandBase and still with stand-in values.

--- src/Commands/PlaceGearRight.cpp
+++ src/Commands/PlaceGearRight.cpp
@@ -1,27 +1,27 @@
-#include "PlaceGearLeft.h"
-#include "Commands/LeftAutonomousTurn1.h"
-#include "Commands/LeftAutonomousTurn2.h"
+#include "PlaceGearRight.h"
+#include "Commands/RightAutonomousTurn1.h"
+#include "Commands/RightAutonomousTurn2.h"
 #include "Commands/MoveBackwardUsingEncoder1.h"
 #include "Commands/MoveBackwardUsingEncoder2.h"
 #include "Commands/VisionTracking.h"
 
-PlaceGearLeft::PlaceGearLeft() {
+PlaceGearRight::PlaceGearRight() {
 
 	//Order of Operations:
 	//All of this is backwards because bot is placed backwards on the field (gear collector in back of robot)
 
-	//- Rotate to the right
-	//- Move backward
 	//- Rotate to the left
+	//- Move backward
+	//- Rotate to the right
 	//- Move backward
 	//- Vision Tracking
 
 
 	Requires(CommandBase::driveBase);
 
-	AddSequential(new LeftAutonomousTurn1());
+	AddSequential(new RightAutonomousTurn1());
 	AddSequential(new MoveBackwardUsingEncoder1());
-	AddSequential(new LeftAutonomousTurn2());
+	AddSequential(new RightAutonomousTurn2());
 	AddSequential(new MoveBackwardUsingEncoder2());
 	AddSequential(new VisionTracking());
 
---
+++ src/Commands/PlaceGearRight.h
@@ -0,0 +1,11 @@
+#ifndef PlaceGearRight_H
+#define PlaceGearRight_H
+
+#include <Commands/CommandGroup.h>
+
+class PlaceGearRight : public CommandGroup {
+public:
+	PlaceGearRight();
+};
+
+#endif  // PlaceGearRight_H
